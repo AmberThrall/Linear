20821c9a59de24431583591be19988e523f4c4e1
Amber Thrall <arthrall@math.arizona.edu>
2021-04-11 16:33:52 -0700

    Some more matrix construction + expanded upon diag

--- src/Construction.h
+++ src/Construction.h
@@ -6,35 +6,180 @@
 #include "Basics.h"
 
 namespace Linear {
-    template <typename T, size_t N>
-    typename std::enable_if<(N>0), SquareMatrix<T,N>>::type Identity() {
-        SquareMatrix<T,N> ret(T(0));
+    template <typename T, size_t N, unsigned int Flags = 0>
+    typename std::enable_if<(N>0), SquareMatrix<T,N,Flags>>::type Identity() {
+        SquareMatrix<T,N,Flags> ret(T(0));
         for (size_t i = 0; i < N; ++i)
             ret(i,i) = T(1);
         return ret;
     }
-    template<typename T>
-    SquareMatrix<T,Dynamic> Identity(size_t n) {
-        SquareMatrix<T,Dynamic> ret(n, n, T(0));
+    template<typename T, unsigned int Flags = 0>
+    SquareMatrix<T,Dynamic,Flags> Identity(size_t n) {
+        SquareMatrix<T,Dynamic,Flags> ret(n, n, T(0));
         for (size_t i = 0; i < n; ++i)
             ret(i,i) = T(1);
         return ret;
     }
-
-    template<typename T, typename... Ts>
-    SquareMatrix<T,sizeof...(Ts)+1> Diag(Complex<T> first, Ts... ts) {
-        SquareMatrix<T,sizeof...(Ts)+1> ret(sizeof...(Ts)+1, sizeof...(Ts)+1, T(0));
-        std::array<Complex<T>,sizeof...(Ts)> arr = {ts...};
-        ret(0,0) = first;
-        for (size_t i = 1; i < sizeof...(Ts); ++i)
-            ret(i,i) = arr[i];
+    template <typename T, size_t M, size_t N, unsigned int Flags = 0>
+    typename std::enable_if<(M>0&&N>0), Matrix<T,M,N,Flags>>::type Zero() {
+        Matrix<T,M,N,Flags> ret(T(0));
         return ret;
+    }
+    template<typename T, unsigned int Flags = 0>
+    Matrix<T,Dynamic,Dynamic,Flags> Zero(size_t nrows, size_t ncols) {
+        Matrix<T,Dynamic,Dynamic,Flags> ret(nrows, ncols, T(0));
+        return ret;
+    }
+    template <typename T, size_t M, size_t N, unsigned int Flags = 0>
+    typename std::enable_if<(M>0&&N>0), Matrix<T,M,N,Flags>>::type One() {
+        Matrix<T,M,N,Flags> ret(T(1));
+        return ret;
+    }
+    template<typename T, unsigned int Flags = 0>
+    Matrix<T,Dynamic,Dynamic,Flags> One(size_t nrows, size_t ncols) {
+        Matrix<T,Dynamic,Dynamic,Flags> ret(nrows, ncols, T(1));
+        return ret;
+    }
+    template <typename T, size_t M, size_t N, unsigned int Flags = 0>
+    typename std::enable_if<(M>0&&N>0), Matrix<T,M,N,Flags>>::type Constant(Complex<T> v) {
+        Matrix<T,M,N,Flags> ret(v);
+        return ret;
+    }
+    template<typename T, unsigned int Flags = 0>
+    Matrix<T,Dynamic,Dynamic,Flags> Constant(size_t nrows, size_t ncols, Complex<T> v) {
+        Matrix<T,Dynamic,Dynamic,Flags> ret(nrows, ncols, v);
+        return ret;
+    }
+
+    template<typename T, size_t M1, size_t N1, unsigned int Flags1,
+            size_t M2, size_t N2, unsigned int Flags2,
+            size_t M3, size_t N3, unsigned int Flags3,
+            size_t M4, size_t N4, unsigned int Flags4>
+    typename std::enable_if<((M1+M3==M2+M4||M1==Dynamic||M2==Dynamic||M3==Dynamic||M4==Dynamic)&&(N1+N2==N3+N4||N1==Dynamic||N2==Dynamic||N3==Dynamic||N4==Dynamic)),
+        Matrix<T,(M1==Dynamic||M2==Dynamic||M3==Dynamic||M4==Dynamic?Dynamic:M1+M3),(N1==Dynamic||N2==Dynamic||N3==Dynamic||N4==Dynamic?Dynamic:N1+N2),Flags1>>::type
+    Block(const Matrix<T,M1,N1,Flags1>& tl, const Matrix<T,M2,N2,Flags2>& tr, const Matrix<T,M3,N3,Flags3>& bl, const Matrix<T,M4,N4,Flags4>& br) {
+        if (tl.NumRows()+bl.NumRows() != tr.NumRows()+br.NumRows() || tl.NumColumns()+tr.NumColumns() != bl.NumColumns()+br.NumColumns())
+            throw "Malformed block matrix. Sizes don't add up equally.";
+        Matrix<T,(M1==Dynamic||M2==Dynamic||M3==Dynamic||M4==Dynamic?Dynamic:M1+M3),(N1==Dynamic||N2==Dynamic||N3==Dynamic||N4==Dynamic?Dynamic:N1+N2),Flags1> ret(tl.NumRows()+bl.NumRows(),tl.NumColumns()+tr.NumColumns());
+        for (size_t r = 0; r < tl.NumRows(); ++r) {
+            for (size_t c = 0; c < tl.NumColumns(); ++c) {
+                ret(r,c) = tl(r,c);
+            }
+        }
+
+        for (size_t r = 0; r < tr.NumRows(); ++r) {
+            for (size_t c = 0; c < tr.NumColumns(); ++c) {
+                ret(r,c+tl.NumColumns()) = tr(r,c);
+            }
+        }
+
+        for (size_t r = 0; r < bl.NumRows(); ++r) {
+            for (size_t c = 0; c < bl.NumColumns(); ++c) {
+                ret(r+tl.NumRows(),c) = bl(r,c);
+            }
+        }
+
+        for (size_t r = 0; r < br.NumRows(); ++r) {
+            for (size_t c = 0; c < br.NumColumns(); ++c) {
+                ret(r+tl.NumRows(),c+bl.NumColumns()) = br(r,c);
+            }
+        }
+        return ret;
+    }
+    template<typename T, size_t M, size_t N, unsigned int Flags, size_t P, size_t Q>
+    typename std::enable_if<((M==P||M==Dynamic||P==Dynamic)&&(N==Q||N==Dynamic||Q==Dynamic)),
+        Matrix<T,(M==Dynamic?Dynamic:M+1),(N==Dynamic?Dynamic:N+1),Flags>>::type
+    Block(const Matrix<T,M,N,Flags>& tl, const Vector<T,P>& tr, const RowVector<T,Q>& bl, Complex<T> br) {
+        return Block(tl, tr, bl, Matrix<T,1,1,Flags>(br));
+    }
+    template<typename T, size_t M, size_t N, unsigned int Flags, size_t P, size_t Q>
+    typename std::enable_if<((M==P||M==Dynamic||P==Dynamic)&&(N==Q||N==Dynamic||Q==Dynamic)),
+        Matrix<T,(M==Dynamic?Dynamic:M+1),(N==Dynamic?Dynamic:N+1),Flags>>::type
+    Block(Complex<T> tl, const RowVector<T,Q>& tr, const Vector<T,P>& bl, const Matrix<T,M,N,Flags>& br) {
+        return Block(Matrix<T,1,1,Flags>(tl), tr, bl, br);
+    }
+
+    template<typename T, size_t M, size_t N, unsigned int Flags1, size_t P, size_t Q, unsigned int Flags2>
+    Matrix<T,M*P,Q*N,Flags1> Kronecker(const Matrix<T,M,N,Flags1>& a, const Matrix<T,P,Q,Flags2>& b) {
+        Matrix<T,M*P,Q*N,Flags1> ret(a.NumRows()*b.NumRows(), a.NumColumns()*b.NumColumns(), T(0));
+        for (size_t r = 0; r < ret.NumRows(); ++r) {
+            for (size_t c = 0; c < ret.NumColumns(); ++c) {
+                ret(r,c) = a(r/b.NumRows(),c/b.NumColumns())*b(r%b.NumRows(),c%b.NumColumns());
+            }
+        }
+        return ret;
+    }
+    template<typename T, size_t M, size_t N, unsigned int Flags1, size_t P, size_t Q, unsigned int Flags2>
+    typename std::enable_if<((M==N||M==Dynamic||N==Dynamic)&&(P==Q||P==Dynamic||Q==Dynamic)), Matrix<T,N*Q,N*Q,Flags1>>::type
+    KroneckerSum(const Matrix<T,M,N,Flags1>& a, const Matrix<T,P,Q,Flags2>& b) {
+        if (!IsSquare(a) || !IsSquare(b))
+            throw "The Kronecker sum is only defined for two square matrices.";
+        SquareMatrix<T,Dynamic,Flags1> In = Identity<T>(a.NumRows());
+        SquareMatrix<T,Dynamic,Flags1> Iq = Identity<T>(b.NumRows());
+        SquareMatrix<T,N*Q,Flags1> first = Kronecker(a, Iq);
+        SquareMatrix<T,N*Q,Flags2> second = Kronecker(In, b);
+        return first+second;
+    }
+
+    template<typename T, size_t M1, size_t N1, unsigned int Flags1, size_t M2, size_t N2, unsigned int Flags2>
+    Matrix<T,(M1==Dynamic||M2==Dynamic?Dynamic:M1+M2),(N1==Dynamic||N2==Dynamic?Dynamic:N1+N2),Flags1> Diag(const Matrix<T,M1,N1,Flags1>& a, const Matrix<T,M2,N2,Flags2>& b) {
+        Matrix<T,(M1==Dynamic||M2==Dynamic?Dynamic:M1+M2),(N1==Dynamic||N2==Dynamic?Dynamic:N1+N2),Flags1> ret(a.NumRows()+b.NumRows(),a.NumColumns()+b.NumColumns(),T(0));
+        for (size_t r = 0; r < ret.NumRows(); ++r) {
+            for (size_t c = 0; c < ret.NumColumns(); ++c) {
+                if (r < a.NumRows() && c < a.NumColumns())
+                    ret(r,c) = a(r,c);
+                else if (r >= a.NumRows() && c >= a.NumColumns())
+                    ret(r,c) = b(r-a.NumRows(),c-a.NumColumns());
+                else
+                    ret(r,c) = T(0);
+            }
+        }
+        return ret;
+    }
+    template<typename T, size_t M, size_t N, unsigned int Flags>
+    Matrix<T,(M==Dynamic?Dynamic:M+1),(N==Dynamic?Dynamic:N+1),Flags> Diag(const Matrix<T,M,N,Flags>& a, Complex<T> b) {
+        Matrix<T,(M==Dynamic?Dynamic:M+1),(N==Dynamic?Dynamic:N+1),Flags> ret(a.NumRows()+1,a.NumColumns()+1,T(0));
+        for (size_t r = 0; r < a.NumRows(); ++r) {
+            for (size_t c = 0; c < a.NumColumns(); ++c) {
+                ret(r,c) = a(r,c);
+            }
+        }
+        ret(ret.NumRows()-1,ret.NumColumns()-1) = b;
+        return ret;
+    }
+    template<typename T, unsigned int Flags=0>
+    Matrix<T,2,2,Flags> Diag(Complex<T> a, Complex<T> b) {
+        Matrix<T,2,2,Flags> ret(T(0));
+        ret(0,0) = a;
+        ret(1,1) = b;
+        return ret;
+    }
+    template<typename T, size_t M1, size_t N1, unsigned int Flags1, size_t M2, size_t N2, unsigned int Flags2, typename... Ts>
+    Matrix<T,Dynamic,Dynamic,Flags1> Diag(const Matrix<T,M1,N1,Flags1>& a, const Matrix<T,M2,N2,Flags2>& b, Ts&&... ts) {
+        Matrix<T,Dynamic,Dynamic,Flags1> m = Diag(a,b);
+        return Diag(m, Matrix<T,Dynamic,Dynamic,Flags1>(std::forward<Ts>(ts))...);
+    }
+    template<typename T, typename... Ts>
+    Matrix<T,Dynamic,Dynamic> Diag(Complex<T> a, Complex<T> b, Ts&&... ts) {
+        Matrix<T,Dynamic,Dynamic> m = Diag(a,b);
+        return Diag(m, (Complex<T>)std::forward<Ts>(ts)...);
     }
     template<typename T>
     SquareMatrix<T,Dynamic> Diag(std::vector<Complex<T>> v) {
+        if (v.size() == 0)
+            throw "Cannot create a diagonal matrix based on a empty list.";
         SquareMatrix<T,Dynamic> ret(v.size(), v.size(), T(0));
         for (size_t i = 0; i < v.size(); ++i)
             ret(i,i) = v[i];
+        return ret;
+    }
+    template<typename T, unsigned int Flags>
+    Matrix<T,Dynamic,Dynamic,Flags> Diag(std::vector<Matrix<T,Dynamic,Dynamic,Flags>> v) {
+        if (v.size() == 0)
+            throw "Cannot create a diagonal matrix based on a empty list.";
+        Matrix<T,Dynamic,Dynamic,Flags> ret = v[0];
+        for (size_t i = 0; i < v.size(); ++i)
+            ret = Diag(ret, v[i]);
         return ret;
     }
     template<typename T, size_t N>
@@ -47,5 +192,17 @@ namespace Linear {
     template<typename T, size_t N>
     SquareMatrix<T,N> Diag(RowVector<T,N> v) {
         return Diag(Transpose(v));
+    }
+
+    template<typename T, unsigned int Flags = 0>
+    SquareMatrix<T,Dynamic,Flags> CompanionMatrix(std::vector<Complex<T>> coeffs) {
+        if (coeffs.size() == 0)
+            throw "Cannot create a 0x0 matrix.";
+        SquareMatrix<T,Dynamic,Flags> ret(coeffs.size(), coeffs.size(), T(0));
+        for (size_t i = 0; i < coeffs.size()-1; ++i)
+            ret(i+1,i) = T(1);
+        for (size_t i = 0; i < coeffs.size(); ++i)
+            ret(i,coeffs.size()-1) = -coeffs[i];
+        return ret;
     }
 }
--- tests/Construction.cpp
+++ tests/Construction.cpp
@@ -3,22 +3,65 @@
 using namespace Linear;
 
 int main() {
-    //MatrixXf eye0 = Identity<float,0>();
-    Matrix3f eye1 = Identity<float,3>();
-    std::cout << "eye1 = " << eye1 << std::endl;
-    MatrixXf eye2 = Identity<float>(2);
-    std::cout << "eye2 = " << eye2 << std::endl;
+    try {
+        //MatrixXf eye0 = Identity<float,0>();
+        Matrix3f eye1 = Identity<float,3>();
+        std::cout << "eye1 = " << eye1 << std::endl;
+        MatrixXf eye2 = Identity<float>(2);
+        std::cout << "eye2 = " << eye2 << std::endl;
 
-    Matrix3f diag1 = Diag<float>(1,2,3);
-    std::cout << "diag1 = " << diag1 << std::endl;
-    MatrixXf diag2 = Diag<float>(std::vector<Complex<float>>({1,2,3}));
-    std::cout << "diag2 = " << diag2 << std::endl;
-    Vector3f v = {1,2,3};
-    Matrix3f diag3 = Diag(v);
-    std::cout << "diag3 = " << diag3 << std::endl;
-    RowVector3f row_v = {1,2,3};
-    Matrix3f diag4 = Diag(row_v);
-    std::cout << "diag4 = " << diag4 << std::endl;
-    SquareMatrix<float,1> diag5 = Diag<float>(1);
-    std::cout << "diag5 = " << diag5 << std::endl;
+        Matrix3f diag1 = Diag<float>(1,2,3);
+        std::cout << "diag1 = " << diag1 << std::endl;
+        MatrixXf diag2 = Diag<float>(std::vector<Complex<float>>({1,2,3}));
+        std::cout << "diag2 = " << diag2 << std::endl;
+        Vector3f v = {1,2,3};
+        Matrix3f diag3 = Diag(v);
+        std::cout << "diag3 = " << diag3 << std::endl;
+        RowVector3f row_v = {1,2,3};
+        Matrix3f diag4 = Diag(row_v);
+        std::cout << "diag4 = " << diag4 << std::endl;
+        MatrixXf diag5 = Diag(eye1, diag1, eye2);
+        std::cout << "diag5 = " << diag5 << std::endl;
+
+        Matrix2f zero1 = Zero<float,2,2,RowMajor>();
+        std::cout << "zero1 = " << zero1 << std::endl;
+        MatrixXf zero2 = Zero<float>(2,3);
+        std::cout << "zero2 = " << zero2 << std::endl;
+
+        Matrix2f one1 = One<float,2,2>();
+        std::cout << "one1 = " << one1 << std::endl;
+        MatrixXf one2 = One<float>(2,3);
+        std::cout << "one2 = " << one2 << std::endl;
+
+        Matrix2f constant1 = Constant<float,2,2>(Complex<float>::i());
+        std::cout << "constant1 = " << constant1 << std::endl;
+        MatrixXf constant2 = Constant<float>(2,3,Complex<float>::i());
+        std::cout << "constant2 = " << constant2 << std::endl;
+
+        Matrix2f kronecker1 = {1,2,3,4};
+        Matrix2f kronecker2 = {0,5,6,7};
+        std::cout << "kronecker(kronecker1,kronecker2) = " << Kronecker(kronecker1,kronecker2) << std::endl;
+        std::cout << "KroneckerSum(kronecker1,kronecker2) = " << KroneckerSum(kronecker1,kronecker2) << std::endl;
+
+        Matrix2f blocktl1 = Constant<float,2,2>(1);
+        Matrix<float,2,3> blocktr1 = Constant<float,2,3>(2);
+        Matrix<float,2,3> blockbl1 = Constant<float,2,3>(3);
+        Matrix2f blockbr1 = Constant<float,2,2>(4);
+        Matrix<float,4,5> block1 = Block(blocktl1, blocktr1, blockbl1, blockbr1);
+        std::cout << "block1 = " << block1 << std::endl;
+        Matrix2f blocktl2 = blocktl1;
+        Vector2f blocktr2 = { 2, 2 };
+        RowVector2f blockbl2 = { 3, 3 };
+        Complexf blockbr2(4);
+        Matrix3f block2 = Block(blocktl2, blocktr2, blockbl2, blockbr2);
+        Matrix3f block3 = Block(blockbr2, blockbl2, blocktr2, blocktl2);
+        std::cout << "block2 = " << block2 << std::endl;
+        std::cout << "block3 = " << block3 << std::endl;
+
+        Matrix3f companion = CompanionMatrix(std::vector<Complexf>({1,2,3}));
+        std::cout << "companion = " << companion << std::endl;
+    }
+    catch (const char* what) {
+        std::cerr << "Error: " << what << std::endl;
+    }
 }
